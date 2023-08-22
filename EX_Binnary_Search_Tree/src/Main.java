1aa4dac4df96488bbbecdd4b8d55a904339a729a
setsuna1995 <kien05071995@gmail.com>
2023-08-22 09:53:29 +0700

    BNS

---
+++ EX_Binnary_Search_Tree/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+    static int binarySearch(int[] list, int key) {
+        int min = 0;
+        int max = list.length - 1;
+        while (max >= min) {
+            int mid = (min + max) / 2;
+            if (key < list[mid]) {
+                max = mid - 1;
+            } else if (key == list[mid]) {
+                return mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));  /* 0 */
+        System.out.println(binarySearch(list, 11)); /* 4 */
+        System.out.println(binarySearch(list, 79)); /*12 */
+        System.out.println(binarySearch(list, 1));  /*-1 */
+        System.out.println(binarySearch(list, 5));  /*-1 */
+        System.out.println(binarySearch(list, 80)); /*-1 */
+    }
+}
